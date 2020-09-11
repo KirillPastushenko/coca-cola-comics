1536760094232fa6e32a758ba651ebeb50d89afc
KirillPastushenko <kpastushenko@gmail.com>
2020-09-11 09:37:56 +0300

    -

--- src/index.js
+++ src/index.js
@@ -1,70 +1,78 @@
-import Vue from 'vue';
-import Vuebar from 'vuebar';
-import * as comicsData from './components/comics-data';
-import './css/style.css';
+import Vue from "vue";
+import Vuebar from "vuebar";
+import * as comicsData from "./components/comics-data";
+import "./css/style.css";
 Vue.use(Vuebar);
 const app = new Vue({
-    el: '#app',
-    data: function () {
-        return {
-            items: comicsData.comicsData,
-            isComicsShow: false,
-            isThumbsActive: false,
-            currComics: {},
-            currImageArr: [],
-            currImageArrId: -1,
-            prev: false,
-            next: true,
-            currCountImages: 0,
-        }
+  el: "#app",
+  data: function() {
+    return {
+      items: comicsData.comicsData,
+      isComicsShow: false,
+      isThumbsActive: false,
+      currComics: {},
+      currImageArr: [],
+      currImageArrId: -1,
+      prev: false,
+      next: true,
+      currCountImages: 0,
+    };
+  },
+  created() {
+    document.addEventListener("keydown", (event) => {
+      if (event.keyCode == 32 || event.keyCode == 39 || event.keyCode == 13) {
+        this.showNextPage();
+      } else if (event.keyCode == 37) {
+        this.showPrevPage();
+      }
+    });
+  },
+  methods: {
+    showThumbs: function() {
+      this.isThumbsActive = !this.isThumbsActive;
     },
-    methods: {
-        showThumbs: function () {
-            this.isThumbsActive = !this.isThumbsActive;
-        },
 
-        showComicPage: function (comicsID) {
-            this.isComicsShow = true;
-            this.currComics = this.items.filter(item => item.id == comicsID)[0];
-            this.currImageArr = [];
-            this.currImageArrId = -1;
-            this.currCountImages = this.currComics.pages.length;
-            this.currImageArr.push(this.currComics.poster);
-        },
+    showComicPage: function(comicsID) {
+      this.isComicsShow = true;
+      this.currComics = this.items.filter((item) => item.id == comicsID)[0];
+      this.currImageArr = [];
+      this.currImageArrId = -1;
+      this.currCountImages = this.currComics.pages.length;
+      this.currImageArr.push(this.currComics.poster);
+    },
 
-        setCurrImageArr: function(id){
-            this.currCountImages = this.currComics.pages.length;
-            this.currImageArrId = id;
-            this.currImageArr = [];
-            this.currImageArr = this.currComics.pages[id].slice();
-            
-        },
+    setCurrImageArr: function(id) {
+      this.currCountImages = this.currComics.pages.length;
+      this.currImageArrId = id;
+      this.currImageArr = [];
+      this.currImageArr = this.currComics.pages[id].slice();
+    },
 
-        beforeEnter: function (el) {
-            el.style.opacity = 0;
-        },
+    beforeEnter: function(el) {
+      el.style.opacity = 0;
+      el.style.transform = "translateY(20px)";
+    },
 
-        enter: function (el, done) {
-            let delay = el.dataset.index * 350;
-            setTimeout(function () {
-                el.style.opacity = 1;
-            }, delay)
-        },
+    enter: function(el, done) {
+      let delay = el.dataset.index * 700;
+      setTimeout(function() {
+        el.style.opacity = 1;
+        el.style.transform = "translateY(0)";
+      }, delay);
+    },
 
-        showPrevPage: function(){
-             this.setCurrImageArr(--this.currImageArrId);
-        },
+    showPrevPage: function() {
+      if (this.currImageArrId > 0)
+        this.setCurrImageArr(--this.currImageArrId);
+    },
 
-        showNextPage: function(){
-            this.setCurrImageArr(++this.currImageArrId);
-        }
-    }
-})
+    showNextPage: function() {
+      if (this.currImageArrId + 1 < this.currCountImages)
+        this.setCurrImageArr(++this.currImageArrId);
+    },
+  },
+});
 
-
-
-setTimeout(function () {
-    document.querySelector('body').classList.remove('firstload');
+setTimeout(function() {
+  document.querySelector("body").classList.remove("firstload");
 }, 50);
-
- 
